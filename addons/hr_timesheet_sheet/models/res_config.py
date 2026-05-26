e7f7342b2dedd4742311c8214699474d33051781
Hichem Nehaoua <hne@qorelis.com>
2026-05-26 10:10:24 +0100

    maj

---
+++ addons/hr_timesheet_sheet/models/res_config.py
@@ -0,0 +1,24 @@
+
+from odoo import fields, models
+
+
+class ResConfig(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    sheet_range = fields.Selection(
+        related="company_id.sheet_range",
+        string="Timesheet Sheet Range",
+        help="The range of your Timesheet Sheet.",
+        readonly=False,
+    )
+
+    timesheet_week_start = fields.Selection(
+        related="company_id.timesheet_week_start",
+        string="Week Start Day",
+        help="Starting day for Timesheet Sheets.",
+        readonly=False,
+    )
+
+    timesheet_sheet_review_policy = fields.Selection(
+        related="company_id.timesheet_sheet_review_policy", readonly=False
+    )
